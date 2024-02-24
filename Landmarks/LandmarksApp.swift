c38f35ddf9bfaed3775dcc10c236f0bc876d34e8
Samir Zafar <88251610+ChaudharySamirZafar@users.noreply.github.com>
2024-02-24 11:20:48 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Samir Zafar on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
